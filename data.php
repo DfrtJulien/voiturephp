71841735992cb59f8dce82204c5e5cbbdfdc0d4f
DfrtJulien <de.freitas.julien78@gmail.com>
2024-09-11 11:14:44 +0200

    fin de l'exercice voiture PHP

--- data.php
+++ data.php
@@ -2,35 +2,34 @@
 
 $cars = array(
      array(
-        'id' => 0,
-        'name' => 'Peugoet 208',
+        'id' => 1,
+        'name' => 'Peugot 208',
         'price' => 20000,
         'picture' => 'voiture1.jpg'
     ),
      array(
-        'id' => 1,
+        'id' => 2,
         'name' => 'Ferrari',
         'price' => 80000,
         'picture' => 'voiture2.jpg'
     ),
      array(
-        'id' => 2,
+        'id' => 3,
         'name' => 'Range rover',
         'price' => 50000,
         'picture' => 'voiture3.jpg'
     ),
      array(
-        'id' => 3,
+        'id' => 4,
         'name' => 'Alpine',
         'price' => 150000,
         'picture' => 'voiture4.jpg'
     ),
      array(
-        'id' => 4,
+        'id' => 5,
         'name' => 'Audi',
         'price' => 90000,
         'picture' => 'voiture5.jpg'
     ),
 );
-    echo var_dump($cars);
 ?>
